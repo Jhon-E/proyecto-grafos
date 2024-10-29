00983423b9296a9c2f9efc23787bd87a4341c035
Jhone <79727940+Jhon-E@users.noreply.github.com>
2024-10-29 13:29:23 -0500

    Delete dijkstra_front directory

--- dijkstra_front/src/components/Loader.jsx
+++
@@ -1,8 +0,0 @@
-export const Loader = ({ text }) => {
-  return (
-    <div className="w-full h-full flex flex-col justify-center items-center">
-      <span className="loading loading-spinner text-neutral loading-lg"></span>
-      <p>{text}</p>
-    </div>
-  );
-};
--- dijkstra_front/src/components/LocationMarker.jsx
+++
@@ -1,9 +0,0 @@
-import { useMapEvents, Marker, Popup } from "react-leaflet";
-
-export const LocationMarker = ({ coords }) => {
-  return coords === null ? null : (
-    <Marker position={coords}>
-      <Popup>ESTAS AQUÍ</Popup>
-    </Marker>
-  );
-};
